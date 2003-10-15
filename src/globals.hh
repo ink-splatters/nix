a72d7edebde44d06c82f28639e81d127c62103fa
Eelco Dolstra <e.dolstra@tudelft.nl>
2003-10-15 12:42:39 +0000

    * Refactoring: move all database manipulation into store.cc. * Removed `--query --generators'.
    Former-commit-id: ebff82222c7b946e70e539389c0027529b6c7ad0

---
+++ src/globals.hh
@@ -0,0 +1,29 @@
+#ifndef __GLOBALS_H
+#define __GLOBALS_H
+
+#include <string>
+
+using namespace std;
+
+/* Path names. */
+
+/* nixStore is the directory where we generally store atomic and
+   derived files. */
+extern string nixStore;
+
+extern string nixDataDir; /* !!! fix */
+
+/* nixLogDir is the directory where we log various operations. */ 
+extern string nixLogDir;
+
+/* nixDBPath is the path name of our Berkeley DB environment. */
+extern string nixDBPath;
+
+
+/* Misc. global flags. */
+
+/* Whether to keep temporary directories of failed builds. */
+extern bool keepFailed;
+
+
+#endif /* !__GLOBALS_H */
