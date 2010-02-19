3df5a72c52c9603954fbaa40c16339a3b26991b9
Eelco Dolstra <e.dolstra@tudelft.nl>
2010-02-19 17:15:22 +0000

    * Foreign key support in SQLite is not a persistent setting, so enable   it at startup. * Implement negative caching.  Now `make check' passes.
    Former-commit-id: 1930570ad96e47de9e8557a7734c7bfd9f36f942

---
+++ tests/referrers.sh
@@ -0,0 +1,37 @@
+source common.sh
+
+# This takes way to long on Cygwin (because process creation is so slow...).
+if test "$system" = i686-cygwin; then exit 0; fi
+
+clearStore
+
+max=2500
+
+reference=$NIX_STORE_DIR/abcdef
+touch $reference
+(echo $reference && echo && echo 0) | $nixstore --register-validity 
+
+echo "making registration..."
+
+for ((n = 0; n < $max; n++)); do
+    storePath=$NIX_STORE_DIR/$n
+    touch $storePath
+    ref2=$NIX_STORE_DIR/$((n+1))
+    if test $((n+1)) = $max; then
+        ref2=$reference
+    fi
+    (echo $storePath && echo && echo 2 && echo $reference && echo $ref2)
+done > $TEST_ROOT/reg_info
+
+echo "registering..."
+
+time $nixstore --register-validity < $TEST_ROOT/reg_info
+
+echo "collecting garbage..."
+ln -sfn $reference "$NIX_STATE_DIR"/gcroots/ref
+time $nixstore --gc
+
+if test "$(sqlite3 ./test-tmp/db/db.sqlite 'select count(*) from Refs')" -ne 0; then
+    echo "referrers not cleaned up"
+    exit 1
+fi
