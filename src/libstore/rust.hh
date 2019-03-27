d3ba1c3869934441b0d735f9fd5441ad588b0c40
Eelco Dolstra <edolstra@gmail.com>
2019-03-27 23:40:35 +0100

    Make <nix/unpack-channel.nix> a builtin builder
    This was the last function using a shell script, so this allows us to
    get rid of tar, coreutils, bash etc.
    
    
    Former-commit-id: 045708db4343174f30f3647776971c852f72a9e8

---
+++ src/libstore/rust.hh
@@ -0,0 +1,44 @@
+#include "serialise.hh"
+
+namespace rust {
+
+// Depending on the internal representation of Rust slices is slightly
+// evil...
+template<typename T> struct Slice
+{
+    T * ptr;
+    size_t size;
+
+    Slice(T * ptr, size_t size) : ptr(ptr), size(size)
+    {
+        assert(ptr);
+    }
+};
+
+struct StringSlice : Slice<char>
+{
+    StringSlice(const std::string & s): Slice((char *) s.data(), s.size()) {}
+};
+
+struct Source
+{
+    size_t (*fun)(void * source_this, rust::Slice<uint8_t> data);
+    nix::Source * _this;
+
+    Source(nix::Source & _this)
+        : fun(sourceWrapper), _this(&_this)
+    {}
+
+    // FIXME: how to propagate exceptions?
+    static size_t sourceWrapper(void * _this, rust::Slice<uint8_t> data)
+    {
+        auto n = ((nix::Source *) _this)->read(data.ptr, data.size);
+        return n;
+    }
+};
+
+}
+
+extern "C" {
+    bool unpack_tarfile(rust::Source source, rust::StringSlice dest_dir);
+}
