31808f10daf590d6c1edcfdfce433d1c3c4c6b2e
Eelco Dolstra <edolstra@gmail.com>
2020-04-10 10:24:09 +0200

    Remove flake 'edition' field
    Future editions of flakes or the Nix language can be supported by
    renaming flake.nix (e.g. flake-v2.nix). This avoids a bootstrap
    problem where we don't know which grammar to use to parse
    flake*.nix. It also allows a project to support multiple flake
    editions, in theory.
    
    
    Former-commit-id: e5ea01c1a8bbd328dcc576928bf3e4271cb55399

---
+++ src/libexpr/flake/call-flake.nix
@@ -0,0 +1,29 @@
+lockFileStr: rootSrc: rootSubdir:
+
+let
+
+  lockFile = builtins.fromJSON lockFileStr;
+
+  allNodes =
+    builtins.mapAttrs
+      (key: node:
+        let
+          sourceInfo =
+            if key == lockFile.root
+            then rootSrc
+            else fetchTree ({ inherit (node.info) narHash; } // removeAttrs node.locked ["dir"]);
+          subdir = if key == lockFile.root then rootSubdir else node.locked.dir or "";
+          flake = import (sourceInfo + (if subdir != "" then "/" else "") + subdir + "/flake.nix");
+          inputs = builtins.mapAttrs (inputName: key: allNodes.${key}) (node.inputs or {});
+          outputs = flake.outputs (inputs // { self = result; });
+          result = outputs // sourceInfo // { inherit inputs; inherit outputs; inherit sourceInfo; };
+        in
+          if node.flake or true then
+            assert builtins.isFunction flake.outputs;
+            result
+          else
+            sourceInfo
+      )
+      lockFile.nodes;
+
+in allNodes.${lockFile.root}
