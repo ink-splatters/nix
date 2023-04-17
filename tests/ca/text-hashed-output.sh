d4384ec95c69403adc3b00b657db74dd221a1eaa
John Ericson <John.Ericson@Obsidian.Systems>
2023-04-17 19:02:45 -0400

    `TextHashMethod` -> `TextIngestionMethod`, gate with XP feature
    I suppose we can use `dynamic-derivations` for the few things we neeed.
    
    
    Former-commit-id: 668377f217c0fa4053d746f7094dfe887e07887c

---
+++ tests/ca/text-hashed-output.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+# Globally enable dynamic-derivations in addition to CA derivations
+enableFeatures "dynamic-derivations"
+
+restartDaemon
+
+# In the corresponding nix file, we have two derivations: the first, named root,
+# is a normal recursive derivation, while the second, named dependent, has the
+# new outputHashMode "text". Note that in "dependent", we don't refer to the
+# build output of root, but only to the path of the drv file. For this reason,
+# we only need to:
+#
+# - instantiate the root derivation
+# - build the dependent derivation
+# - check that the path of the output coincides with that of the original derivation
+
+drv=$(nix-instantiate ./text-hashed-output.nix -A root)
+nix show-derivation "$drv"
+
+drvDep=$(nix-instantiate ./text-hashed-output.nix -A dependent)
+nix show-derivation "$drvDep"
+
+out1=$(nix-build ./text-hashed-output.nix -A dependent --no-out-link)
+
+nix path-info $drv --derivation --json | jq
+nix path-info $out1 --derivation --json | jq
+
+test $out1 == $drv
