roblabla/nix-1

Organize content addressing, use `SourceAccessor` with `Store::addToStore`
Co-authored-by: Robert Hensing <roberth@users.noreply.github.com>

src/libutil/file-content-address.cc
@@ -1,0 +1,49 @@
+#include "file-content-address.hh"
+#include "archive.hh"
+
+namespace nix {
+
+void dumpPath(
+    SourceAccessor & accessor, const CanonPath & path,
+    Sink & sink,
+    FileIngestionMethod method,
+    PathFilter & filter)
+{
+    switch (method) {
+    case FileIngestionMethod::Flat:
+        accessor.readFile(path, sink);
+        break;
+    case FileIngestionMethod::Recursive:
+        accessor.dumpPath(path, sink, filter);
+        break;
+    }
+}
+
+
+void restorePath(
+    const Path & path,
+    Source & source,
+    FileIngestionMethod method)
+{
+    switch (method) {
+    case FileIngestionMethod::Flat:
+        writeFile(path, source);
+        break;
+    case FileIngestionMethod::Recursive:
+        restorePath(path, source);
+        break;
+    }
+}
+
+
+HashResult hashPath(
+    SourceAccessor & accessor, const CanonPath & path,
+    FileIngestionMethod method, HashType ht,
+    PathFilter & filter)
+{
+    HashSink sink { ht };
+    dumpPath(accessor, path, sink, method, filter);
+    return sink.finish();
+}
+
+}

ink-splatters/nix

Former-commit-id: dfc876531f269950a4e183a4f77a813c421d7d64

src/libutil/file-content-address.cc
@@ -1,0 +1,49 @@
+#include "file-content-address.hh"
+#include "archive.hh"
+
+namespace nix {
+
+void dumpPath(
+    SourceAccessor & accessor, const CanonPath & path,
+    Sink & sink,
+    FileIngestionMethod method,
+    PathFilter & filter)
+{
+    switch (method) {
+    case FileIngestionMethod::Flat:
+        accessor.readFile(path, sink);
+        break;
+    case FileIngestionMethod::Recursive:
+        accessor.dumpPath(path, sink, filter);
+        break;
+    }
+}
+
+
+void restorePath(
+    const Path & path,
+    Source & source,
+    FileIngestionMethod method)
+{
+    switch (method) {
+    case FileIngestionMethod::Flat:
+        writeFile(path, source);
+        break;
+    case FileIngestionMethod::Recursive:
+        restorePath(path, source);
+        break;
+    }
+}
+
+
+HashResult hashPath(
+    SourceAccessor & accessor, const CanonPath & path,
+    FileIngestionMethod method, HashAlgorithm ht,
+    PathFilter & filter)
+{
+    HashSink sink { ht };
+    dumpPath(accessor, path, sink, method, filter);
+    return sink.finish();
+}
+
+}