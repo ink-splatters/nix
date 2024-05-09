ink-splatters/nix

commit 6b651c48f29127b1df9342a0c48e8eca1201721a

Tarball fetcher: Use the content-addressed Git cache
Backported from the lazy-trees branch.


Former-commit-id: cabee9815239af426cece729cb765810b8a716ce

src/libfetchers/tarball.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-3b7709440b2306225da0575a206a012d2f0b3629
+e3b1fbe565aa5e88d23642501849be1b9bf29226

src/libcmd/common-eval-args.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-58f04e2259b174bce75d9878032706b31ec83b72
+444ff81c9a670da18df00509e12988acd6024406

src/libexpr/eval.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-4919ac358e06da085e7c55494c75208883b1d8ba
+133d02f592b616b014892c360c7b7ecee89392a1

src/libexpr/primops/fetchTree.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-1997d55137cb4c5c8e403eaae7ebbdfa337b2ac7
+01a43e3fd11deaf9e04c0933329adf8f69a04cd6

src/libfetchers/git-utils.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-4f034e9d4608ca0b077003648cf2e7459a7ba686
+f216b6e108e6878b170844947f114de2e852bf29

src/libfetchers/git-utils.hh.REMOVED.git-id
@@ -1,1 +1,1 @@
-5f68d26a7738555a41141e7fd2a7da3d76a4493d
+fbb2d947b45c887e3048da27cec31231019e7ab8

src/libfetchers/tarball.hh
@@ -2,11 +2,13 @@
 
 #include "types.hh"
 #include "path.hh"
+#include "hash.hh"
 
 #include <optional>
 
 namespace nix {
 class Store;
+struct InputAccessor;
 }
 
 namespace nix::fetchers {
@@ -28,16 +30,18 @@ DownloadFileResult downloadFile(
 
 struct DownloadTarballResult
 {
-    StorePath storePath;
+    Hash treeHash;
     time_t lastModified;
     std::optional<std::string> immutableUrl;
+    ref<InputAccessor> accessor;
 };
 
+/**
+ * Download and import a tarball into the Git cache. The result is the
+ * Git tree hash of the root directory.
+ */
 DownloadTarballResult downloadTarball(
-    ref<Store> store,
     const std::string & url,
-    const std::string & name,
-    bool locked,
     const Headers & headers = {});
 
 }

src/libstore/filetransfer.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-ebfae346f300255a006e52e6f47303dd07ba1cb6
+bab21bf517bb9a2759e8a88db6cce04f671adbbb

commit 65c9c2ec458e03bc598f23bae8b75975c346ee08

Handle zip files containing symlinks
In streaming mode, libarchive doesn't handle symlinks in zip files
correctly. So write the entire file to disk so libarchive can access
it in random-access mode.

Fixes #10649. This was broken in 6b651c48f29127b1df9342a0c48e8eca1201721a [formerly cabee9815239af426cece729cb765810b8a716ce].


Former-commit-id: 9951e14ae085c818a11e28c0d530a111c1e2ab86

src/libfetchers/tarball.cc.REMOVED.git-id
@@ -1,1 +1,1 @@
-e19b1850560b9e3be5328202aa201fc42728a772
+d03ff82cedd6d74f954cfeafa88dcb3b340c7eb2

tests/functional/flakes/prefetch.sh
@@ -1,0 +1,6 @@
+source common.sh
+
+# Test symlinks in zip files (#10649).
+path=$(nix flake prefetch --json file://$(pwd)/tree.zip | jq -r .storePath)
+[[ $(cat $path/foo) = foo ]]
+[[ $(readlink $path/bar) = foo ]]

tests/functional/local.mk.REMOVED.git-id
@@ -1,1 +1,1 @@
-65ab20f9a4f56a7e00996542eb825a81e1d88364
+17664cd7e3cc208a252fca9d8031294199c11d17