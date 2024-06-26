cba4b57e1ea6d70e1dc0c9cf6bcf6380f9f4848e
Robert Hensing <robert@roberthensing.nl>
2024-06-26 03:33:30 +0200

    components.nix: Simplify with scope
    Former-commit-id: 74b9b77c9f05099c2bb207890d0728455a85efa7

---
+++ packaging/components.nix
@@ -0,0 +1,29 @@
+{pkgs, stdenv, versionSuffix}: scope:
+let
+  inherit (scope) callPackage;
+in
+
+# This becomes the pkgs.nixComponents attribute set
+{
+  inherit stdenv versionSuffix;
+  libseccomp = pkgs.libseccomp-nix;
+  boehmgc = pkgs.boehmgc-nix;
+  libgit2 = pkgs.libgit2-nix;
+  busybox-sandbox-shell = pkgs.busybox-sandbox-shell or pkgs.default-busybox-sandbox-shell;
+
+  nix = callPackage ../package.nix { };
+
+  nix-util = callPackage ../src/libutil/package.nix { };
+
+  nix-util-test-support = callPackage ../tests/unit/libutil-support/package.nix { };
+
+  nix-util-test = callPackage ../tests/unit/libutil/package.nix { };
+
+  nix-util-c = callPackage ../src/libutil-c/package.nix { };
+
+  nix-store = callPackage ../src/libstore/package.nix { };
+
+  nix-fetchers = callPackage ../src/libfetchers/package.nix { };
+
+  nix-perl-bindings = callPackage ../src/perl/package.nix { };
+}
